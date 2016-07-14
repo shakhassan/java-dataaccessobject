f2e8d63e155ad135317084c2bac4eca4be8d574f
rjirwanshah rjhassan <rjirwanshah@rjirwanshah.local>
2016-07-14 10:41:15 +0800

    dao template

---
+++ src/main/java/my/winapp/dao/DaoPatternDemo.java
@@ -0,0 +1,32 @@
+/** 
+ * http://www.tutorialspoint.com/design_pattern/data_access_object_pattern.htm 
+ * Student.java = Model Object or Value Object
+ * StudentDao.java = Data Access Object Interface
+ * StudentDaoImpl.java = Concrete class implementing above interface
+ * 
+ * **/
+
+package my.winapp.dao;
+
+public class DaoPatternDemo {
+
+	public static void main(String[] args) {
+	      StudentDao studentDao = new StudentDaoImpl();
+
+	      //print all students
+	      for (Student student : studentDao.getAllStudents()) {
+	         System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+	      }
+
+
+	      //update student
+	      Student student =studentDao.getAllStudents().get(0);
+	      student.setName("Michael");
+	      studentDao.updateStudent(student);
+
+	      //get the student
+	      studentDao.getStudent(0);
+	      System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");		
+	   }
+
+}
